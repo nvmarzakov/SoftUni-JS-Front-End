edb8b03e23480f7286348740e37648e56b15b5bf
nvmarzakov <nikolay.marzakov@gmail.com>
2023-11-08 17:50:09 +0200

    finish exercises

---
+++ JS-Arrays-&-Strings/Exercises/08-Pascal-Case-Splitter.js
@@ -0,0 +1,11 @@
+function solve(text) {
+    let wordsRegex = /[A-Z][a-z]*/g;
+    let wordsArray = text.match(wordsRegex);
+
+    let resultString = wordsArray.join(', ');
+
+    console.log(resultString);
+
+}
+
+solve('SplitMeIfYouCanHaHaYouCantOrYouCan');
